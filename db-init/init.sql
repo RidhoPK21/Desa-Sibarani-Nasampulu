83ed78b61e01311a199de075e9840cbbea6db76f
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2026-03-02 19:46:18 +0700

    first commit

---
+++ db-init/init.sql
@@ -0,0 +1,6 @@
+-- Database db_auth sudah dibuat secara otomatis oleh docker-compose.yml
+-- Di bawah ini kita membuat database untuk service lainnya:
+
+CREATE DATABASE db_info;
+CREATE DATABASE db_content;
+CREATE DATABASE db_analytic;
